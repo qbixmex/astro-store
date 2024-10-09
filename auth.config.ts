04e1a3f83ec5acd4556392b9c9b2bcffd3a73a6d
Daniel González Briseño <qbixmex@gmail.com>
2024-10-09 03:00:56 -0700

    AuthJs - Credentials Provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'auth-astro';
+import Credentials from "@auth/core/providers/credentials";
+import { db, eq, User } from 'astro:db';
+import bcrypt from 'bcryptjs';
 
 export default defineConfig({
   providers: [
-    // TODO: Add authentication providers
+    Credentials({
+      authorize: async ({ email, password }) => {
+        //* Find user by email.
+        const [user] = await db.
+          select()
+          .from(User)
+          .where(
+            eq(User.email, email as string)
+          );
+
+        //* Check if user exists in DB.
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        //* Check if passwords match.
+        if (!bcrypt.compareSync(password as string, user.password)) {
+          throw new Error("Invalid credentials");
+        }
+
+        //* Remove password from user object
+        const {password: _, ...authUser} = user;
+
+        return authUser;
+      },
+    }),
   ],
 });
