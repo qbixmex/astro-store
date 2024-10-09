3288e1cadcd96f509105cd128654195f82ccf052
Daniel González Briseño <qbixmex@gmail.com>
2024-10-09 00:21:35 -0700

    Initial Assets and Layouts, Pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
+
+import { User } from "@/interfaces"; 
+
+declare namespace App {
+  interface Locals {
+    isLoggedIn: boolean;
+    user: User | null;
+  }
+}
+
+interface ImportMetaEnv {
+  readonly PUBLIC_DOMAIN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
