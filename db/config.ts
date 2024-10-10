eb8e1aaf4051f21ff7b4c48770a17ddd6cf60d9f
Daniel González Briseño <qbixmex@gmail.com>
2024-10-10 07:43:58 -0700

    Product Images, Seed Tables and Product Database Structure

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,7 @@
 import { defineDb, defineTable, column } from 'astro:db';
 
+//* https://astro.build/db/config
+
 const User = defineTable({
   columns: {
     id: column.text({ primaryKey: true, unique: true }),
@@ -7,6 +9,8 @@ const User = defineTable({
     email: column.text({ unique: true }),
     password: column.text(),
     createdAt: column.date({ default: new Date() }),
+
+    //* Relationship
     role: column.text({ references: () => Role.columns.id }),
   },
 });
@@ -18,10 +22,37 @@ const Role = defineTable({
   }
 });
 
-// https://astro.build/db/config
+const Product = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    stock: column.number(),
+    slug: column.text(),
+    price: column.number(),
+    sizes: column.text(),
+    type: column.text(),
+    tags: column.text(),
+    title: column.text(),
+    description: column.text(),
+    gender: column.text(),
+
+    //* Relationship
+    user: column.text({ references: () => User.columns.id }),
+  },
+});
+
+const ProductImage = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    productId: column.text({ references: () => Product.columns.id }),
+    image: column.text(),
+  },
+});
+
 export default defineDb({
   tables: {
     User,
     Role,
+    Product,
+    ProductImage,
   }
 });
