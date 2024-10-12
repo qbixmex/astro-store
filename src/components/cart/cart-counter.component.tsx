2e58bc2890329168ae5eb8293cdb4deef57de1fc
Daniel González Briseño <qbixmex@gmail.com>
2024-10-12 13:30:21 -0700

    Share data between islands, Nanostores

--- src/components/cart/cart-counter.component.tsx
+++ src/components/cart/cart-counter.component.tsx
@@ -1,12 +1,33 @@
-import styles from './cart-counter.module.css';
+import { useStore } from "@nanostores/react";
+import { itemsInCart } from "@/store";
 import CartIcon from './cart-icon.component';
+import EmptyCartIcon from './empty-cart-icon.component';
+import styles from './cart-counter.module.css';
+import { useEffect } from "react";
+import { CartCookiesClient } from "@/utils";
 
 const CartCounter = () => {
+  const $itemsInCart = useStore(itemsInCart);
+
+  
+  useEffect(() => {
+    const cart = CartCookiesClient.getCart();
+    itemsInCart.set(cart.length);
+  }, [])
+
   return (
-    <a href="/cart" className={styles.cartLink}>
-      <span className={styles.cartCounter}>5</span>
-      <CartIcon size={28} />
-    </a>
+    ($itemsInCart > 0) ? (
+      <a href="/cart" className={styles.cartLink}>
+        <span className={styles.cartCounter}>
+          {$itemsInCart}
+        </span>
+        <CartIcon size={28} />
+      </a>
+    ) : (
+      <span className={styles.emptyCart}>
+        <EmptyCartIcon size={28} className={styles.disabledIcon} />
+      </span>
+    )
   );
 };
 
