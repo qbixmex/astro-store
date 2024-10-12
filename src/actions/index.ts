b6cb53756a585e03e26c1dcbf94688818fa0685d
Daniel González Briseño <qbixmex@gmail.com>
2024-10-12 10:13:43 -0700

    LoadProducts from Cart

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,12 @@
 import { loginUser, logoutSession, registerUser } from './auth';
 import { getProductsByPage, getProductBySlug } from './products';
+import loadProductsFromCart from './products/load-products-from-cart.action';
 
 export const server = {
   //* Products
   getProductsByPage,
   getProductBySlug,
+  loadProductsFromCart,
 
   //* Auth
   loginUser,
